4ba72f616fa9a894209e88a302949a91f2d0452c
Subin Kumar <isubinkr@icloud.com>
2024-03-13 00:52:04 +0530

    construct MainContainer with movie lists

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,23 +2,21 @@ import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
 
 const Browse = () => {
   useNowPlayingMovies();
+  useTopRatedMovies();
+  usePopularMovies();
+  useUpcomingMovies();
 
   return (
     <div>
       <Header />
       <MainContainer />
       <SecondaryContainer />
-
-      {/* Main Container
-          - Video Bg
-          - Video Title
-        Secondary Container
-          - Movies List * n
-            - Cards * n
-      */}
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,3 +15,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlM2YzYmFiZDdlMTA1M2U3NjUyOGQ0ZTBjMGZkZjQ3MiIsInN1YiI6IjY1ZWUxZDcxYmExMzFiMDE2MmFiYjdjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6D4GoD-ET-SUdolboA5X8kXvYoL1OWBHTAoFWBv3DyY",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
